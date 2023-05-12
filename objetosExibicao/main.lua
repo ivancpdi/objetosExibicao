8031bb338cb6bdc0fc93cfd6d5523fc454aeb1b5
Ivan Dutra Goetze <133060521+ivancpdi@users.noreply.github.com>
2023-05-12 10:53:17 -0300

    Exercicio aula 4 (adicionar objetos)

--- objetosExibicao/main.lua
+++ objetosExibicao/main.lua
@@ -16,23 +16,31 @@
    -- exercicio pikachu
     local pikachu = display.newImageRect ("imagens/pikachu.png", 1191*0.25, 1254*0.25)
 
-    pikachu.x = display.contentCenterX
-    pikachu.y = display.contentCenterY
+    pikachu.x = display.contentCenterX - 230
+    pikachu.y = display.contentCenterY  + 75
 
     -- imagem não centralizada
-    local charmander = display.newImageRect ("imagens/charmander.png", 507*0.25, 492*0.25)
+    local charmander = display.newImageRect ("imagens/charmander.png", 507*0.20, 492*0.20)
 
-    charmander.x = 200
-    charmander.y = 450
+    charmander.x = 600
+    charmander.y = 400
    
     -- Criando um retangulo
     -- Comandos: display.newRect (localizaçao x, localização y, largura, altura)
     local retangulo = display.newRect(750, 380, 100, 100, 70)
 
     -- ciruclo = display.newCircle (x, y, raio)
-    local ciruclo = display.newCircle (150, 80, 50)
-
-    -- local mystery = display.newImageRect ("imagens/mystery.png", 1280, 720)
+    local circulo = display.newCircle (150, 80, 40)
+    
 
     -- Adicionar o mistery encima do retangulo
+    local mystery = display.newImageRect ("imagens/mystery.png", 493*0.25, 506*0.25)
+    mystery.x = display.contentCenterX + 50
+    mystery.y = display.contentCenterY + 193
+    
+    retangulo.x = display.contentCenterX + 65
+    retangulo.y = display.contentCenterY + 300
+
     -- Adicionar o chamander na diagonal do circulo (para baixo)
+    circulo.x = 500
+    circulo.y = 300
